e9e97a524690d6a4b665700248d8608b549a2995
Ahmad Jumadi <ahmadjumadi61@guru.smk.belajar.id>
2025-11-28 09:08:16 +0700

    fix: enforce https on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +22,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        $url = parse_url(url('/'));
+        if($url['scheme'] == 'https' || $this->app->environment('production')){
+            URL::forceScheme('https');
+        }
+        Carbon::setLocale(LC_TIME, $this->app->getLocale());
     }
 }
